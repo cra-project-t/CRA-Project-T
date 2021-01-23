e60056ab74ff32ed87d7b9f593d9d51a0f34306b
Harim <74362214+achel003@users.noreply.github.com>
2021-01-23 22:59:33 +0900

    SearchGroup틀 제작

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { CssBaseline } from "@material-ui/core";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import HisnetLogin from "./components/HisnetLogin";
+import SearchGroup from "./components/SearchGroup";
 
 const AuthOkay = ({ children }) => {
   const [auth, loading, error] = useAuthState(firebase.auth());
@@ -31,6 +32,7 @@ const App = () => {
               <Route path="/:engName/edit" exact component={EditPurchase} /> */}
             <Route path="/" exact component={HomePage} />
             <Route path="/hisnetlogin" exact component={HisnetLogin} />
+            <Route path="/group/:id" component={SearchGroup} />
           </Switch>
         </Router>
       </AuthOkay>
