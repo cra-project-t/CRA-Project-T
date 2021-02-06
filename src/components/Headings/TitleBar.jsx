352176c660c9ce0ff5c356178a852823d95c8424
Harim <74362214+achel003@users.noreply.github.com>
2021-02-07 01:30:43 +0900

    [그룹추가] 저장 및 들어가기

--- src/components/Headings/TitleBar.jsx
+++ src/components/Headings/TitleBar.jsx
@@ -18,9 +18,10 @@ import SearchFriends from "../SearchFriends";
 import { userStore } from "../../stores/userStore";
 import Notifications from "./Notifications";
 import AddNotif from "../AddNotif";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import AddGroup from "../AddGroup";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1,
   },
@@ -114,7 +115,7 @@ export default function PrimarySearchAppBar() {
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = (event) => {
+  const handleProfileMenuOpen = event => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -127,7 +128,7 @@ export default function PrimarySearchAppBar() {
     handleMobileMenuClose();
   };
 
-  const handleMobileMenuOpen = (event) => {
+  const handleMobileMenuOpen = event => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
@@ -216,6 +217,7 @@ export default function PrimarySearchAppBar() {
               {/* <Button>
                 <Link to="/addnotif">공지추가페이지</Link>
               </Button> */}
+              <AddGroup button={<Button>그룹추가</Button>} />
               <AddNotif button={<Button>공지추가</Button>} />
               <SearchFriends button={<Button>친구추가</Button>} />
             </div>
@@ -231,7 +233,7 @@ export default function PrimarySearchAppBar() {
               <IconButton
                 aria-label={`show ${notificationCount} new notifications`}
                 color="inherit"
-                onClick={(event) => setNotificationEl(event.currentTarget)}
+                onClick={event => setNotificationEl(event.currentTarget)}
               >
                 <Badge badgeContent={notificationCount} color="secondary">
                   <NotificationsIcon />
